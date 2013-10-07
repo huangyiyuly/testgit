dc7518e62262b1022f8e0a56ebffddf435eb33ec
Huzi.Wang <olives.wh@gmail.com>
2013-10-07 22:39:57 +0800

    Update GitTest.java

--- src/com/yuly/git/GitTest.java
+++ src/com/yuly/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("hello git...");
 		System.out.println("I'm from the server...");
+		System.out.println("Huzi.Wang come here");
 	}
 }
